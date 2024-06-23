22204373c56f31ab4bbb20da590105c9613f9927
FrancisBrokering <francisbrokering@gmail.com>
2024-06-23 11:09:43 -0700

    Fixed Freestyle instructions

---
+++ NuPhoneBeta/components/AudioPlayButton.swift
@@ -0,0 +1,179 @@
+import SwiftUI
+import AVFoundation
+
+struct AudioPlayButton: View {
+//    private var audioURL: URL
+    private var callId: String
+    @State private var player: AVPlayer?
+    @State private var isPlaying = false
+    @State private var currentTime: Double = 0
+    @State private var totalTime: Double = 0
+    @State private var timeObserver: Any?
+    @State private var isLoading: Bool = true
+    
+    // For slider and time display
+    var currentTimeString: String {
+        return formatTime(seconds: currentTime)
+    }
+    
+    var totalTimeString: String {
+        return formatTime(seconds: totalTime)
+    }
+    
+//    init(audioURL: URL) {
+//        self.audioURL = audioURL
+//    }
+    
+    init(callId: String) {
+        self.callId = callId
+    }
+    
+    var body: some View {
+        HStack {
+            if self.isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    .frame(width: 30, height: 30) 
+                    .background(
+                        Circle()
+                            .fill(Color("AccentColor"))
+                            .frame(width: 30, height: 30)
+                    )
+                    .disableWithOpacity(true)
+            }
+            else {
+                Button(action: togglePlayPause) {
+                    Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30) // Ensure Button has the same frame
+                }
+            }
+            Slider(value: $currentTime, in: 0...totalTime, onEditingChanged: sliderChanged)
+                .onAppear {
+                    let progressCircleConfig = UIImage.SymbolConfiguration(scale: .small)
+                    UISlider.appearance()
+                        .setThumbImage(UIImage(systemName: "circle.fill",
+                                               withConfiguration: progressCircleConfig), for: .normal)
+                }
+            Text("\(currentTimeString)/\(totalTimeString)")
+                .font(.footnote)
+            //                        .foregroundColor(Color.gray)
+            
+        }
+        //        .background(.gray)
+        .padding(20)
+        .background(.ultraThinMaterial, in:
+                        RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .clipShape(Capsule())
+        .shadow(color: Color("shadow").opacity(0.1), radius: 5, x: 0, y: 5)
+        //        .cornerRadius(10)
+        .onAppear {
+            self.setupAudio(callId: callId)
+        }
+        .onDisappear {
+            if let observer = timeObserver {
+                player?.removeTimeObserver(observer)
+                timeObserver = nil
+            }
+            player?.pause()
+            isPlaying = false
+            currentTime = 0
+            player?.seek(to: .zero)
+        }
+    }
+    
+    private func setupAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Failed to set up audio session for playback: \(error)")
+        }
+    }
+
+    
+    private func setupAudio(callId: String) {
+        setupAudioSession()
+        //        self.player = AVPlayer(url: audioURL)
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let subAccountApiKey = UserManager.shared.subAccountApiKey {
+                self.isLoading = true
+                print("SUBACCOUNT")
+                let headers: [String: String] = [
+                    "X-API-KEY": subAccountApiKey
+                ]
+                CallsAPI.getAudioUrl(callId: callId) { audioURL in
+                    if audioURL != nil {
+                            let asset = AVURLAsset(url: audioURL!)
+                            print("ASSETS", asset)
+                            let playerItem = AVPlayerItem(asset: asset)
+                            print("Player Item", playerItem)
+                            DispatchQueue.main.async {
+                            self.player = AVPlayer(playerItem: playerItem)
+                            self.totalTime = player?.currentItem?.asset.duration.seconds ?? 0
+                            
+                            // Add Periodic Time Observer
+                            let interval = CMTime(seconds: 0.1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+                            timeObserver = player?.addPeriodicTimeObserver(forInterval: interval, queue: .main) { time in
+                                self.currentTime = time.seconds
+                            }
+                            
+                            // Notification for when the audio finishes playing
+                            NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player?.currentItem, queue: .main) { _ in
+                                self.isPlaying = false
+                                self.player?.seek(to: .zero)
+                                self.currentTime = 0
+                            }
+                                self.isLoading = false
+                        }
+                    } else {
+                        self.isLoading = false
+                    }
+                }
+            }
+            else {
+                self.isLoading = false
+            }
+        }
+    }
+    
+    private func togglePlayPause() {
+        if isPlaying {
+            player?.pause()
+        } else {
+            player?.play()
+        }
+        isPlaying.toggle()
+    }
+    
+    private func sliderChanged(editingStarted: Bool) {
+        if editingStarted {
+            // If the user starts dragging the slider, pause the player to allow smooth seeking.
+            if isPlaying {
+                player?.pause()
+            }
+        } else {
+            // Seek to the new time once the user stops dragging the slider.
+            let newTime = CMTime(seconds: currentTime, preferredTimescale: 600)
+            player?.seek(to: newTime) { _ in
+                // Only play if the player was playing before user started dragging the slider.
+                if self.isPlaying {
+                    self.player?.play()
+                }
+            }
+        }
+    }
+    
+    private func downloadAudio() {
+        // Implement audio download functionality here
+    }
+    
+    private func formatTime(seconds: Double) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = seconds >= 3600 ? [.hour, .minute, .second] : [.minute, .second]
+        return formatter.string(from: TimeInterval(seconds)) ?? "00:00"
+    }
+}
+
--- NuPhoneBeta/pages/agentView/instructions/FreestyleInstructions.swift
+++ NuPhoneBeta/pages/agentView/instructions/FreestyleInstructions.swift
@@ -4,67 +4,20 @@ struct FreestyleInstructionsView: View {
     @Binding var agent: Agent
     @Binding var editedValue: String
     @Binding var isEditingFreestyleInstructions: Bool
-//    @Binding var isGuidedSelected: Bool
-//    @Binding var isFreestyleSelected: Bool
     @ObservedObject var accountManager = AccountManager.shared
-//    @State private var displaySubscriptionSheet: Bool = false
-    
-//    private func swapRadio() {
-//        self.isGuidedSelected = false
-//        self.isFreestyleSelected = true
-//        agent.prompt.instructions.instruction_type = .freestyle
-//    }
     
     var body: some View {
         VStack {
             VStack(alignment: .center, spacing: 20) {
-//                VStack(alignment: .leading, spacing: 10){
-//                    HStack(alignment: .center) {
-//                        HStack(alignment: .center, spacing: 10) {
-//                            RadioButton(selected: $isFreestyleSelected)
-//                            Text("Freestyle")
-//                                .font(.headline)
-//                        }
-//                        
-//                        
-//                        if accountManager.plan == "free" {
-//                            PremiumIcon()
-//                        }
-//                        Spacer()
-//                    }
-//                    .contentShape(Rectangle())
-//                    .onTapGesture {
-//                        if accountManager.plan == "free" {
-//                            withAnimation(.spring()) {
-//                                displaySubscriptionSheet = true
-//                            }
-//                        }
-//                        else {
-//                            swapRadio()
-//                        }
-//                    }
-//                    Text("Provide open-ended instructions to your assistant")
-//                        .subTextStyle()
-//                }
                 Text(agent.prompt.instructions.freestyle_instruction )
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .background(.white)
                     .contentShape(Rectangle())
                     .onTapGesture {
-//                        if accountManager.plan == "free" {
-//                            withAnimation(.spring()) {
-//                                displaySubscriptionSheet = true
-//                            }
-//                        }
-//                        else {
-                            isEditingFreestyleInstructions.toggle()
-                            editedValue = agent.prompt.instructions.freestyle_instruction
-//                        }
+                        isEditingFreestyleInstructions.toggle()
+                        editedValue = agent.prompt.instructions.freestyle_instruction
                     }
-                //                    .opacity(accountManager.plan == "free" ? 0.5 : 1)
-                //                    .disableWithOpacity(accountManager.plan == "free")
                     .fakeTextAreaStyle()
-//                    .opacity(accountManager.plan == "free" ? 0.5 : 1)
             }
         }
         .sheet(isPresented: $isEditingFreestyleInstructions) {
@@ -84,8 +37,5 @@ struct FreestyleInstructionsView: View {
             .presentationCornerRadius(20)
 
         }
-//        .sheet(isPresented: $displaySubscriptionSheet) {
-//            PaywallView(clickedOnProFeature: true,  isDisplaySheet: $displaySubscriptionSheet)
-//        }
     }
 }
